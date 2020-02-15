f58b0bc1caeff2b9e8b15d631c7edad95ee8058e
Tshepo <tshepo.sibiya@gmail.com>
2020-02-15 19:24:53 +0200

    minor update on returned data.

--- src/app/overview/overview.page.ts
+++ src/app/overview/overview.page.ts
@@ -11,22 +11,31 @@ export class OverviewPage implements OnInit {
 
   city: string;
   _data: any;
+  _hasError = false;
   showData = false;
+  _message: string;
   cityObj = {
     city: '',
+    country: '',
     timezone: '',
 
   }
   constructor(private fb: FormBuilder, private weathersvc: WeatherService) { }
 
   search() {
-    
+    this._hasError = false;
     this.weathersvc.getWeatherByCity(this.city).subscribe((response) => {
+      
       this.cityObj = {
         city: response.name,
-        timezone: response.sys.country,
+        timezone: response.timezone,
+        country: response.sys.country
       }
       this.showData = true;
+    }, (error) => {
+      console.log(error.message);
+      this._hasError = true;
+      this._message = error.message;
     });
   }
 
